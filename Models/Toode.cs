d6a82af6f871ecdd25c5cbd68584da31bec77019
FantomTchi7 <vladislav.kudriashev@gmail.com>
2025-09-30 14:45:57 +0300

    Follow guide until frontend.

---
+++ Models/Toode.cs
@@ -0,0 +1,18 @@
+﻿namespace Veeb_TARpv23.Models
+{
+    public class Toode
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+
+        public Toode(int id, string name, double price, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            IsActive = isActive;
+        }
+    }
+}
